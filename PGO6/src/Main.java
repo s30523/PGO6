655db9523920863e3b7bf235a496e776639e4b88
s30523 <162446714+s30523@users.noreply.github.com>
2024-05-08 03:37:17 +0200

    Main.java

--- PGO6/src/Main.java
+++ PGO6/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 =new Student("Kaja", "Ambroziak", "ambroziak@wp.pl", "Warsaw, Piekna 12", "333-322-222", new Date(2004, 4, 11));
+        Student s1 =new Student("Kaja", "Ambroziak", "ambroziak@wp.pl", "Warsaw", "333-322-222", new Date(2004, 4, 11));
     
         Student s2 =new Student ("Jan", "Kowalski","kowalski@gmail.com", "Lublin", "123-345-567", new Date(2002, 2, 21));
 
